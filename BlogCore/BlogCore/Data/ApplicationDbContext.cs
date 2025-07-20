9a8dc42a4aa3580fcdd30e5282d719affe004d1a
DeeEmDy <dy-lanarias@hotmail.comcls>
2025-07-20 11:54:23 -0600

    Project BlogCore creation files, first commit

---
+++ BlogCore/BlogCore/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogCore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+
+    //Aquí van todos los modelos que se vayan a utilizar en la aplicación.
+}
