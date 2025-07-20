b97e67739bc6a0dd8d36faf29effa9ac2d18a121
DeeEmDy <dy-lanarias@hotmail.com>
2025-07-20 12:33:20 -0600

    Add project files for AccesoDatos, Models, and Utils
    Created new .NET 8.0 class library projects for BlogCore.AccesoDatos, BlogCore.Models, and BlogCore.Utils. These projects will help organize data access, model definitions, and utility functions in the solution.

--- BlogCore/BlogCore/obj/Debug/net8.0/BlogCore.AssemblyInfo.cs
+++ BlogCore/BlogCore/obj/Debug/net8.0/BlogCore.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3782b52e822553acff189ac2691039f4e157764")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
